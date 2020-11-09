c03aadbee09815f7be093fcab9a408aade2523f9
Peter J. Schroeder <peterjschroeder@gmail.com>
2020-11-09 18:59:13 -0500

    hnb files are not utf8, which causes phnb to fail to load the tree.

--- phnb/io.py
+++ phnb/io.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import datetime
 
 def get_tree_xml_from_disk():
-  return ET.parse(config.tree_file).getroot()
+  return ET.parse(config.tree_file, parser=ET.XMLParser(encoding='iso-8859-1')).getroot()
 
 def save_tree_to_disk(root_node):
   xmltree = convert_tree_to_xml(root_node)
